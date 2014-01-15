289bb41087b40a8580ea9d12849ebadda83cb562
andrew_keely <andrew.keely@nuance.com>
2014-01-14 21:07:24 -0500

    Adding comments

--- helmet-unit-nads-api/app/repository/GameRepository.scala
+++ helmet-unit-nads-api/app/repository/GameRepository.scala
@@ -1,10 +1,34 @@
 package repository
 
+import scala.util.Try
 import api._
 
+/**
+ * Access the games.
+ */
 trait GameRepository {
 
-    def find(): Iterator[Game]
-    def findOne(id: Int): Option[Game]
-  	def addEntry(id: Int, entry: Entry): Unit
+  /**
+   * Get all of the games.
+   * 
+   * @return All of the games.
+   */
+  def find(): Iterator[Game]
+  
+  /**
+   * Get one game.
+   * 
+   * @param id The game identifier.
+   * @return The game, or None if the game does not exist.
+   */
+  def findOne(id: String): Option[Game]
+  
+  /**
+   * Add a drawing or sentence to the game.
+   * 
+   * @param id The game to add the entry to.
+   * @param entry The drawing or sentence to add.
+   * @retun The updated game.
+   */
+  def addEntry(id: String, entry: Entry): Try[Game]
 }
